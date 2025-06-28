1239501ef49c0e30bbc2fbd2b9c60cc9f3a4bf97
feliperdamaceno <feliperdamaceno@sent.com>
2025-06-28 19:26:45 +0100

    feat: include store safeguards and improve tests

--- lib/internal/store.test.ts
+++ lib/internal/store.test.ts
@@ -1,6 +1,7 @@
 import { afterEach, describe, expect, it, vi } from 'vitest'
 
 import { defineStore } from '@/internal/store'
+import { isCustomEvent } from '@/internal/utils'
 
 describe('defineStore', () => {
   afterEach(() => {
@@ -30,10 +31,31 @@ describe('defineStore', () => {
       name: 'john'
     }))
 
+    const want = 'value'
+    let got = store.get((state) => state.unchanged)
+    expect(got).to.be.equal(want)
+
     store.set(() => ({ name: 'doe' }))
 
-    const want = 'value'
-    const got = store.get((state) => state.unchanged)
+    got = store.get((state) => state.unchanged)
+    expect(got).to.be.equal(want)
+  })
+
+  it('should not set keys that are not in the original state', () => {
+    const store = defineStore('test', () => ({
+      name: 'john'
+    }))
+
+    // @ts-expect-error: test only
+    store.set(() => ({ age: 25 }))
+
+    let want: string | undefined = undefined
+    // @ts-expect-error: test only
+    let got = store.get((state) => state.age)
+    expect(got).to.be.equal(want)
+
+    want = 'john'
+    got = store.get((state) => state.name)
     expect(got).to.be.equal(want)
   })
 
@@ -45,5 +67,21 @@ describe('defineStore', () => {
 
     store.set(() => ({ name: 'doe' }))
     expect(handler).toBeCalled()
+  })
+
+  it('should send changed state in emitted custom event detail when state changes', () => {
+    const store = defineStore('test', () => ({ name: 'john' }))
+
+    const want = 'doe'
+    let got: string | undefined
+
+    document.addEventListener('syncrate:test', (event) => {
+      if (isCustomEvent(event)) {
+        got = event.detail
+      }
+    })
+
+    store.set(() => ({ name: 'doe' }))
+    expect(got).to.be.equal(want)
   })
 })
--- lib/internal/store.ts
+++ lib/internal/store.ts
@@ -18,12 +18,10 @@ type EventOptions = {
 }
 
 export function defineStore<T extends InitialState>(
-  key: string,
+  name: string,
   setter: StateSetter<T>,
   options: EventOptions = { bubbles: true, cancelable: true, composed: true }
 ): Store<T> {
-  const listeners = new Set<StateGetter<T>>()
-
   const state = new Proxy(setter(), {
     get: (target, property, receiver): T[keyof T] => {
       return Reflect.get(target, property, receiver)
@@ -31,31 +29,36 @@ export function defineStore<T extends InitialState>(
 
     set: (target, property, value, receiver) => {
       const previous = target[property as keyof T]
-      const success = Reflect.set(target, property, value, receiver)
+      const hasChanged = Reflect.set(target, property, value, receiver)
 
-      if (success && previous !== value) {
-        const event = new CustomEvent(`syncrate:${key}`, options)
-        document.dispatchEvent(event)
-        listeners.forEach((listener) => {
-          console.log('value updated')
-          listener(value)
+      if (hasChanged && previous !== value) {
+        const event = new CustomEvent(`syncrate:${name}`, {
+          ...options,
+          detail: state[property as keyof T]
         })
+        document.dispatchEvent(event)
       }
 
-      return success
+      return hasChanged
     }
   })
 
   return {
     get: (getter: StateGetter<T>) => {
-      if (!listeners.has(getter)) listeners.add(getter)
       return getter(state)
     },
+
     set: (setter: StateSetter<Partial<T>>) => {
       const store = setter()
       const entries = Object.entries(store) as StoreEntries<T>
+
       entries.forEach(([key, value]) => {
-        state[key] = value
+        if (Object.keys(state).includes(String(key))) {
+          return (state[key] = value)
+        }
+
+        const error = `key "${String(key)}" does not exist on store "${name}"`
+        return console.error(error)
       })
     }
   }
